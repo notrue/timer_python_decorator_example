640401fb0698c84d8e794300d1cc2a485d42dda7
notrue <yoni.bukchin@gmail.com>
2019-04-14 21:59:02 +0300

    fix

--- timer.py
+++ timer.py
@@ -2,17 +2,17 @@
 from time import time
 def timer(func):
     '''wrapper'''
-        def f(*args, **kwargs):
-            '''
-            timer decorator
-            separates additional behavior from the main functionality.
-            provides mechanism of extending functionality without modifying 
-            the business logic itself 
-            '''
-            before = time()
-            rv = func(*args, **kwargs)
-            after = time()
-            print 'elapsed', after - before
-            return rv
+    def f(*args, **kwargs):
+        '''
+        timer decorator
+        separates additional behavior from the main functionality.
+        provides mechanism of extending functionality without modifying 
+        the business logic itself 
+        '''
+        before = time()
+        rv = func(*args, **kwargs)
+        after = time()
+        print 'elapsed', after - before
+        return rv
     return f
 
